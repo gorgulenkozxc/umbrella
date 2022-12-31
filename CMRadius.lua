dd45c18c65f50b759814252cda5888edc5022a38
ssrankedghoul <gorgulenkozxc@gmail.com>
2022-12-31 14:22:18 +0200

    Changed CM Radius to more efficient way of drawing

--- CMRadius.lua
+++ CMRadius.lua
@@ -7,23 +7,55 @@ Menu.AddMenuIcon(path, '~/Custom/freezingField.png')
 -- https://cartierfam.xyz/storage/freezingField.png
 
 local freezingField = 'modifier_crystal_maiden_freezing_field'
-Engine.ExecuteCommand('dota_range_display 0')
+local circle
+local ulting = false
+local localHero = Heroes.GetLocal()
+
+function cr.OnGameStart()
+    localHero = Heroes.GetLocal()
+end
+
+function cr.OnUpdate()
+    if not Menu.IsEnabled(cr.enabled) then return end
+    if not ulting then return end
+    cr.DrawCircle()
+end
+
+function cr.CreateCircle()
+    circle = Particle.Create("particles\\ui_mouseactions\\drag_selected_ring.vpcf", Enum.ParticleAttachment.PATTACH_WORLDORIGIN, localHero)
+    Particle.SetControlPoint(circle, 0, Entity.GetAbsOrigin(localHero))
+    Particle.SetControlPoint(circle, 1, Vector(120, 120, 255))
+    Particle.SetControlPoint(circle, 2, Vector(900, 255, 255))
+end
+
+function cr.DrawCircle()
+    Particle.SetControlPoint(circle, 0, Entity.GetAbsOrigin(localHero))
+    Particle.SetControlPoint(circle, 1, Vector(120, 120, 255))
+    Particle.SetControlPoint(circle, 2, Vector(900, 255, 255))
+end
+
+function cr.DestroyCircle()
+    Particle.Destroy(circle)
+end
 
 function cr.OnModifierCreate(ent, mod)
     if not Menu.IsEnabled(cr.enabled) then return end
     if Modifier.GetName(mod) ~= freezingField then return end
-    Engine.ExecuteCommand('dota_range_display 810')
+    cr.CreateCircle()
+    ulting = true
 end
 
 function cr.OnModifierDestroy(ent, mod)
     if not Menu.IsEnabled(cr.enabled) then return end
     if Modifier.GetName(mod) ~= freezingField then return end
-    Engine.ExecuteCommand('dota_range_display 0')
+    ulting = false
+    cr.DestroyCircle()
 end
 
 function cr.OnMenuOptionChange(option, oldValue, newValue)
     if option == cr.enabled and newValue == 0 then
-        Engine.ExecuteCommand('dota_range_display 0')
+        ulting = false
+        cr.DestroyCircle()
     end
 end
 
---
+++ CMRadius_naive.lua
@@ -0,0 +1,30 @@
+local cr = {}
+
+local path = {'Custom', 'Freezing Field Radius Naive'}
+
+cr.enabled = Menu.AddOptionBool(path, 'Show', false)
+Menu.AddMenuIcon(path, '~/Custom/freezingField.png')
+-- https://cartierfam.xyz/storage/freezingField.png
+
+local freezingField = 'modifier_crystal_maiden_freezing_field'
+Engine.ExecuteCommand('dota_range_display 0')
+
+function cr.OnModifierCreate(ent, mod)
+    if not Menu.IsEnabled(cr.enabled) then return end
+    if Modifier.GetName(mod) ~= freezingField then return end
+    Engine.ExecuteCommand('dota_range_display 810')
+end
+
+function cr.OnModifierDestroy(ent, mod)
+    if not Menu.IsEnabled(cr.enabled) then return end
+    if Modifier.GetName(mod) ~= freezingField then return end
+    Engine.ExecuteCommand('dota_range_display 0')
+end
+
+function cr.OnMenuOptionChange(option, oldValue, newValue)
+    if option == cr.enabled and newValue == 0 then
+        Engine.ExecuteCommand('dota_range_display 0')
+    end
+end
+
+return cr
